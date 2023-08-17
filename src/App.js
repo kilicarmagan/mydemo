9414b15c1eef393dc1d48c000d21ce78321ed876
kilicarmagan <kilicarmagan@gmail.com>
2023-08-17 21:48:05 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './index.css';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is app.js</h1>
-    </div>
+   <>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Project" element={<Project/>}/>
+    <Route path="/Contact" element={<Contact/>}/>
+    <Route path="/About" element={<About/>}/>
+   </Routes>
+   </>
   );
 }
 
